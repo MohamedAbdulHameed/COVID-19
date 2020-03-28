4af700fb2e326116a70fbdc46efb8c638a1766f6
Muhammad Anwar <42128552+MuAnwar96@users.noreply.github.com>
2020-03-28 21:57:45 +0200

    Update statistical-model.py

--- statistical-model.py
+++ statistical-model.py
@@ -6,8 +6,12 @@ from scipy.optimize import curve_fit
 def exponential(x, a, b):
     return a*n.exp(b*x)
 
+# Fitting to a 2nd degree polynomial
+def polynomial(x, a, b, c):
+    return a*x**2+b*x+c
+
 # Current cases
-cases = n.array([126, 166, 196, 210, 256, 285, 294, 327, 366, 402, 456, 495, 536]) # For each new day, add the number of cases for that day to this array.
+cases = n.array([126, 166, 196, 210, 256, 285, 294, 327, 366, 402, 456, 495, 536, 576]) # For each new day, add the number of cases for that day to this array.
 day = n.arange(1, len(cases)+1, 1)
 
 # Expectations a week later
@@ -24,12 +28,12 @@ case_model = n.array(u)
 day_model = n.arange(len(cases), len(cases)+8, 1)
 
 # Expectations a week earlier
-past_expectations = n.array([1144, 930, 709, 902, 910]) # Data are input from the Google Sheets Analysis
+past_expectations = n.array([1144, 930, 709, 902, 910, 809, 867, 948, 1008, 1139, 1071, 1265, 1324]) # Data are input from the Google Sheets Analysis
 day_past_exp = n.arange(9, len(past_expectations)+9, 1)
 
 # Curve fitting
 fit_parameters1, covariances = curve_fit(exponential, day, cases) # Fitting current cases
-fit_parameters2, covariances = curve_fit(exponential, day_past_exp, past_expectations) # Fitting cases expected a week earlier
+fit_parameters2, covariances = curve_fit(polynomial, day_past_exp, past_expectations) # Fitting cases expected a week earlier
 
 a = p.subplot() # Was called because it has the ability to delete top and right frameline from the plot
 
@@ -43,7 +47,7 @@ a.plot(day_model[7], case_model[7], "Dg")
 
 # Plotting cases expected a week earlier
 a.plot(day_past_exp, past_expectations, "Dg")
-a.plot(day_past_exp, exponential(day_past_exp, *fit_parameters2), "--c", label = "Expected Cases a Week Earlier: %.1f $e^{%.2f}$" % tuple(fit_parameters2))
+a.plot(day_past_exp, polynomial(day_past_exp, *fit_parameters2), "--c", label = "Expected Cases a Week Earlier: %.2f $x^{2}$ + %.2f x + %.2f" % tuple(fit_parameters2))
 
 p.legend()
 p.xlabel("Day")
@@ -57,9 +61,6 @@ for i, text in enumerate(cases):
 
 for i, text in enumerate(past_expectations):
     p.annotate(text, (day_past_exp[i], past_expectations[i]), textcoords = "offset points", xytext = (0, 10), ha = "center")
-
-# Writing the number of cases expected a week later
-p.annotate(str(int(round(case_model[7]))), (day_model[7], case_model[7]), textcoords = "offset points", xytext = (0, 10), ha = "center")
 
 # Deleting the top and right framelines
 a.spines['right'].set_visible(False)
